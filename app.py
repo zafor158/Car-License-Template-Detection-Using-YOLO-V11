8517c14d95f05e5cf851b7a67c7eee95958b93a7
Md.Abu Zafor <98481506+zafor158@users.noreply.github.com>
2024-10-26 13:49:45 +0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from PIL import Image
 from ultralytics import YOLO
 
 # Load your trained YOLO model
-model_path = "zafor158/Car-License-Template-Detection-Using-YOLO-V11/best_model.pt"
+model_path = "Car-License-Template-Detection-Using-YOLO-V11/best_model.pt"
 model = YOLO(model_path)
 
 st.title("License Plate Detection App")
